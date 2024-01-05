1657b5954958a738feba1e0e1ace497f3f704562
Changju Hwang <103680963+ChangJu-Hwang@users.noreply.github.com>
2024-01-05 10:45:00 +0900

    Revert "Generate VO Cones and visualize"

--- build/velocity_obstacle_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/changju/cpp_ws/subgoal_generator/build/velocity_obstacle_test[1]_tests.cmake")
-  include("/home/changju/cpp_ws/subgoal_generator/build/velocity_obstacle_test[1]_tests.cmake")
-else()
-  add_test(velocity_obstacle_test_NOT_BUILT velocity_obstacle_test_NOT_BUILT)
-endif()
--- include/subgoal_generator/vertex.h
+++ include/subgoal_generator/vertex.h
@@ -10,8 +10,6 @@ namespace SubgoalGenerator::DynamicGraph
     class Vertex
     {
     public:
-        Vertex() {}
-
         Vertex(std::string _name, const Pose &_current, const Pose &_goal)
         {
             name_ = _name;
